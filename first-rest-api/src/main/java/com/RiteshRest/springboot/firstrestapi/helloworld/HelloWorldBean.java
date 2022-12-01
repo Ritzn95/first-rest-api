e24b759753b94b0f7739e550c54b638b66618c36
Ritesh <81361554+Ritzn95@users.noreply.github.com>
2022-12-01 13:58:14 +0530

    Add files via upload

---
+++ first-rest-api/src/main/java/com/RiteshRest/springboot/firstrestapi/helloworld/HelloWorldBean.java
@@ -0,0 +1,23 @@
+package com.RiteshRest.springboot.firstrestapi.helloworld;
+
+public class HelloWorldBean {
+	public HelloWorldBean(String message) {
+		super();
+		this.message = message;
+	}
+	
+	private String message;
+
+
+	public String getMessage() {
+		return message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+	
+	
+
+}
---
+++ first-rest-api/src/test/java/com/RiteshRest/springboot/firstrestapi/survey/SurveyResourceIT.java
@@ -0,0 +1,90 @@
+package com.RiteshRest.springboot.firstrestapi.survey;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class SurveyResourceIT {
+	
+	private static String SPECIFIC_QUESTION_URL = "/surveys/Survey1/questions/Question1";
+	
+	private static String GENERIC_QUESTIONS_URL = "/surveys/Survey1/questions/";
+	
+	@Autowired
+	private TestRestTemplate template;
+
+	String str = """
+			
+			{
+			  "id": "Question1",
+			  "description": "Most Popular Cloud Platform Today",
+			  "options": [
+			    "AWS",
+			    "Azure",
+			    "Google Cloud",
+			    "Oracle Cloud"
+			  ],
+			  "correctAnswer": "AWS"
+			}
+			
+			""";
+		
+	@Test
+	void retrieveSpecificSurveyQuestion_basicScenario() throws JSONException {
+		
+		ResponseEntity<String> responseEntity = template.getForEntity(SPECIFIC_QUESTION_URL, String.class);
+
+		String expectedResponse =
+				"""
+				{
+					"id":"Question1",
+					"description":"Most Popular Cloud Platform Today",
+					"correctAnswer":"AWS"
+				}
+				""";
+
+		assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+		assertEquals("application/json", responseEntity.getHeaders().get("Content-Type").get(0));
+		
+		JSONAssert.assertEquals(expectedResponse, responseEntity.getBody(), false);
+		 
+	}
+	
+	@Test
+	void retrieveAllSurveyQuestions_basicScenario() throws JSONException {
+		
+		ResponseEntity<String> responseEntity = template.getForEntity(GENERIC_QUESTIONS_URL, String.class);
+
+		String expectedResponse =
+				"""
+						[
+						  {
+						    "id": "Question1"
+						  },
+						  {
+						    "id": "Question2"
+						  },
+						  {
+						    "id": "Question3"
+						  }
+						]
+				
+				""";
+
+		assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+		assertEquals("application/json", responseEntity.getHeaders().get("Content-Type").get(0));
+		
+		JSONAssert.assertEquals(expectedResponse, responseEntity.getBody(), false);
+		 
+	}
+
+}
